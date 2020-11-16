44cf585783f5f02cbd5aecbe11692b45b3412b8f
fuckinrobotics <60086412+fuckinrobotics@users.noreply.github.com>
2020-11-16 05:34:05 +0300

    Everything is done, first prototype

--- app/src/main/java/com/example/djiryzetellocommander/SimpleRunFragment.java
+++ app/src/main/java/com/example/djiryzetellocommander/SimpleRunFragment.java
@@ -1,5 +1,6 @@
 package com.example.djiryzetellocommander;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class SimpleRunFragment extends Fragment {
+    Thread Thread1 = null;
+    String SERVER_IP;
+    int SERVER_PORT;
+
     // Инициализация view
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,6 +41,10 @@ public class SimpleRunFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         // инициализируем View для отображения списка
         Button btnBackSimpleRun = view.findViewById(R.id.btn_back_simple_run);
+        SERVER_IP = "";
+        SERVER_PORT = 9000;
+        Thread1 = new Thread(new Thread1());
+        Thread1.start();
         btnBackSimpleRun.setText(R.string.btn_back_simple_run);
         btnBackSimpleRun.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +57,8 @@ public class SimpleRunFragment extends Fragment {
         btnClockwise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String n = "cv 180".trim();
+                new Thread(new Thread3(n)).start();
             }
         });
         Button btnLand = view.findViewById(R.id.btn_land);
@@ -48,6 +66,8 @@ public class SimpleRunFragment extends Fragment {
         btnLand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String n = "land".trim();
+                new Thread(new Thread3(n)).start();
             }
         });
         Button btnTakeOff = view.findViewById(R.id.btn_take_off);
@@ -55,7 +75,61 @@ public class SimpleRunFragment extends Fragment {
         btnTakeOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String n = "takeoff".trim();
+                new Thread(new Thread3(n)).start();
             }
         });
+    }
+
+    private PrintWriter output;
+    private BufferedReader input;
+
+    class Thread1 implements Runnable {
+        public void run() {
+            Socket socket;
+            try {
+                socket = new Socket(SERVER_IP, SERVER_PORT);
+                output = new PrintWriter(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                new Thread(new Thread2()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message != null) {
+
+                    } else {
+                        Thread1 = new Thread(new Thread1());
+                        Thread1.start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class Thread3 implements Runnable {
+        private String message;
+
+        Thread3(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            output.write(message);
+            output.flush();
+
+        }
     }
 }
