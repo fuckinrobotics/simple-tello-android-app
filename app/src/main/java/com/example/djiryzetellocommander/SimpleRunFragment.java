3b53e004c059084a636abd380e9f4fa01f5786e1
fuckinrobotics <60086412+fuckinrobotics@users.noreply.github.com>
2020-11-18 02:07:48 +0300

    connect with error

--- app/src/main/java/com/example/djiryzetellocommander/SimpleRunFragment.java
+++ app/src/main/java/com/example/djiryzetellocommander/SimpleRunFragment.java
@@ -19,11 +19,15 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class SimpleRunFragment extends Fragment {
-    Thread Thread1 = null;
-    String SERVER_IP;
-    int SERVER_PORT;
-
+    public JTello drone = new JTello();
     // Инициализация view
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,10 +45,8 @@ public class SimpleRunFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         // инициализируем View для отображения списка
         Button btnBackSimpleRun = view.findViewById(R.id.btn_back_simple_run);
-        SERVER_IP = "";
-        SERVER_PORT = 9000;
-        Thread1 = new Thread(new Thread1());
-        Thread1.start();
+       // String n1 = "Command";
+     //   new Thread(new Thread3(n1)).start();
         btnBackSimpleRun.setText(R.string.btn_back_simple_run);
         btnBackSimpleRun.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,8 +59,11 @@ public class SimpleRunFragment extends Fragment {
         btnClockwise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String n = "cv 180".trim();
-                new Thread(new Thread3(n)).start();
+                try {
+                    drone.cw(180);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         Button btnLand = view.findViewById(R.id.btn_land);
@@ -66,8 +71,11 @@ public class SimpleRunFragment extends Fragment {
         btnLand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String n = "land".trim();
-                new Thread(new Thread3(n)).start();
+                try {
+                    drone.land();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         Button btnTakeOff = view.findViewById(R.id.btn_take_off);
@@ -75,61 +83,17 @@ public class SimpleRunFragment extends Fragment {
         btnTakeOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String n = "takeoff".trim();
-                new Thread(new Thread3(n)).start();
-            }
-        });
-    }
-
-    private PrintWriter output;
-    private BufferedReader input;
-
-    class Thread1 implements Runnable {
-        public void run() {
-            Socket socket;
-            try {
-                socket = new Socket(SERVER_IP, SERVER_PORT);
-                output = new PrintWriter(socket.getOutputStream());
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                new Thread(new Thread2()).start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    class Thread2 implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
                 try {
-                    final String message = input.readLine();
-                    if (message != null) {
-
-                    } else {
-                        Thread1 = new Thread(new Thread1());
-                        Thread1.start();
-                        return;
-                    }
+                    drone.connect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    drone.takeOff();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        }
-    }
-
-    class Thread3 implements Runnable {
-        private String message;
-
-        Thread3(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public void run() {
-            output.write(message);
-            output.flush();
-
-        }
+        });
     }
 }
